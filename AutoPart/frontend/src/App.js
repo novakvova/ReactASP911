16361c8d90f158dc5d9a9bc36d1b2173bdeefad7
novakvova <novakvova@gmail.com>
2021-09-12 13:19:33 +0300

    .gitignore Fixed

---
+++ AutoPart/frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './components/header';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <>
+       <Header/>
+        <div className="container">
+          <h1>Hello --</h1>
+          <h2>Вона зможе</h2>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
